78cf83f4b9cae19e28877cb746ef1dfb8edee004
mchatzis <michatzis@gmail.com>
2021-03-02 11:39:25 +0000

    Revert "create db.json"
    This reverts commit 1182f8c63c4974f58219fa5acca72afa56e33dff.

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,13 +3,10 @@ public class Program {
     public static void main(String[] args) throws Exception{
 
         //First initialize the structure
-
-        //Initialize the Importer
         IReader reader = new TextReader ("chat.txt");
         IParser parser = new Parser();
         IImporter importer = new Importer(reader,parser);
 
-        //Initialize the Processor
         IFiltererBy[] filtererBys = new IFiltererBy[] {
                 new FiltererByUser("bob"),
                 new FiltererByKeyword("pie")
@@ -18,15 +15,13 @@ public class Program {
         IReporter reporter = new Reporter(true);
         IProcessor processor = new Processor(filterer,reporter);
 
-        //Initialize the Exporter
         IConverter<Product,String> converter = new ProductToJsonConverter();
         IWriter writer = new TextFileWriter("out.json");
         IExporter exporter = new Exporter(converter,writer);
 
-        //Initialize the Manager of all
         Manager manager = new Manager(importer, processor, exporter);
 
-        //Finally, run business logic
+        //Finally, do business.
         manager.doBusiness();
     }
 }
