1182f8c63c4974f58219fa5acca72afa56e33dff
mchatzis <michatzis@gmail.com>
2021-03-02 09:08:08 +0000

    create db.json

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,10 +3,13 @@ public class Program {
     public static void main(String[] args) throws Exception{
 
         //First initialize the structure
+
+        //Initialize the Importer
         IReader reader = new TextReader ("chat.txt");
         IParser parser = new Parser();
         IImporter importer = new Importer(reader,parser);
 
+        //Initialize the Processor
         IFiltererBy[] filtererBys = new IFiltererBy[] {
                 new FiltererByUser("bob"),
                 new FiltererByKeyword("pie")
@@ -15,13 +18,15 @@ public class Program {
         IReporter reporter = new Reporter(true);
         IProcessor processor = new Processor(filterer,reporter);
 
+        //Initialize the Exporter
         IConverter<Product,String> converter = new ProductToJsonConverter();
         IWriter writer = new TextFileWriter("out.json");
         IExporter exporter = new Exporter(converter,writer);
 
+        //Initialize the Manager of all
         Manager manager = new Manager(importer, processor, exporter);
 
-        //Finally, do business.
+        //Finally, run business logic
         manager.doBusiness();
     }
 }
